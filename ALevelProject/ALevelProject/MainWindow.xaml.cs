93664c143875e87f6c42d0ff54c5bb8108ef3b52
morrisons90 <morrisons233@gmail.com>
2018-03-05 20:39:28 +0000

    Start Of Feature-Search

--- ALevelProject/ALevelProject/MainWindow.xaml.cs
+++ ALevelProject/ALevelProject/MainWindow.xaml.cs
@@ -23,6 +23,16 @@ namespace ALevelProject
         public MainWindow()
         {
             InitializeComponent();
+
+            //Get Instruction text from database
+            var instructions = (string)SQLI.SingleResultQuery("SELECT data FROM GUITexts WHERE ref='MainInstructions'")[0]; //This function returns a ArrayList but the query only returns 1 item so [0]
+            var instructionsP = instructions.Replace("\\n",Environment.NewLine);
+            instructionsText.DataContext = instructionsP;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ ALevelProject/ALevelProject/SQLI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace ALevelProject
+{
+    static class SQLI
+    {
+        private static SqlConnection NewConnection()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Josh\source\repos\ALevelProject\ALevelProject\ALevelProject\Database1.mdf;Integrated Security=True");
+            conn.Open();
+
+            return conn;
+        }
+        public static ArrayList SingleResultQuery(String query)
+        {
+            SqlConnection conn = NewConnection();
+
+            SqlCommand command = new SqlCommand(query, conn);                      
+            try
+            {
+                var result = command.ExecuteReader();
+                result.Read();
+                ArrayList results = new ArrayList();
+                for(var i=0; i<result.FieldCount; i++) //from the index 0 to the number of fields -1 (for base 0)
+                {
+                    results.Add(result.GetValue(i));
+                }
+                return results;
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine("Error With Query");
+                return (new ArrayList {"Fail"});
+            }
+
+        }
+    }
+}
